6403caf276336ef6987b6197a01ac884cdbb4bbd
Ilavarasi1 <sheethal.jan@gmail.com>
2021-01-08 10:33:55 +0200

    he

---
+++ test_per.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Go to containing folder
+cd /home/jovyan/work/
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+
+pwd
+echo "******************"
+ls -lah
+
